5da98e5dc38ba027f51eea307f9942be09befac2
shahi <shubhushahi96@gmail.com>
2025-04-27 18:52:48 +0530

    Renamed invoiceApi.js to InvoiceApi.js and updated imports

--- src/assets/helper/InvoiceApi.js
+++ src/assets/helper/InvoiceApi.js
@@ -11,7 +11,6 @@ export const invoiceInsert = async (invoiceData) => {
       },
     });
     const { data } = res;
-
     if (data.success) {
       handleSuccess(data.message);
       return data;
